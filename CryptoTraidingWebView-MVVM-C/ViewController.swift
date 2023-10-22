fadde53ffdf9cd9d219161cd46e19b39bb1a8d95
Ilia <ilya2491@gmail.com>
2023-10-22 12:39:24 +0700

    Initial Commit

---
+++ CryptoTraidingWebView-MVVM-C/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  CryptoTraidingWebView-MVVM-C
+//
+//  Created by i0240 on 22.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
